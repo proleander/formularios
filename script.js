f2b3dce365e9bf70bb18dc7c388b198234c595c4
Leandro Silva <98911107+proleander@users.noreply.github.com>
2022-09-27 17:32:18 -0300

    Add files via upload
    incrementando funcionanlidades

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 
 
 function calcular(){
-    var valor1 = parseInt(document.getElementById('txt1').value, 10);
-    var valor2 = parseInt(document.getElementById('txt2').value, 10);
-    document.getElementById('result').value = valor1 - valor2;
+    var valor1 = parseInt(document.getElementById('txt1').value, 0).toFixed(2);;
+    var valor2 = parseInt(document.getElementById('txt2').value, 0).toFixed(2);;
+    document.getElementById('result').value = (valor1 - valor2).toFixed(2);
+}
+
+function MascaraMoeda(i) {
+    var v = i.value.replace(/\D/g, '');
+    v = (v / 100).toFixed(2) + '';
+    v = v.replace(".", ",");
+    v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+    v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+    i.value = v;
 }
