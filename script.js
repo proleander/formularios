dac7e90730e6e260d4594814cc4cd3dfc75ffc30
Leandro Silva <98911107+proleander@users.noreply.github.com>
2022-09-29 00:20:48 -0300

    ajustando flex-box

--- script.js
+++ script.js
@@ -2,16 +2,7 @@
 
 
 function calcular(){
-    var valor1 = parseInt(document.getElementById('txt1').value, 0).toFixed(2);;
-    var valor2 = parseInt(document.getElementById('txt2').value, 0).toFixed(2);;
-    document.getElementById('result').value = (valor1 - valor2).toFixed(2);
-}
-
-function MascaraMoeda(i) {
-    var v = i.value.replace(/\D/g, '');
-    v = (v / 100).toFixed(2) + '';
-    v = v.replace(".", ",");
-    v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
-    v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
-    i.value = v;
+    var valor1 = parseFloat(document.getElementById('txt1').value, 10).toFixed(3);
+    var valor2 = parseFloat(document.getElementById('txt2').value, 10).toFixed(3);
+    document.getElementById('result').value = (valor1 - valor2).toFixed(3);
 }
